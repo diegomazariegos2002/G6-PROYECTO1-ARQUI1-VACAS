81559f36067df69be9e904b9a1378fa451895b59
Estuardo Son <2986622050101@ingenieria.usac.edu.gt>
2022-12-15 15:02:21 -0600

    Cambio a 16 parqueos

--- Proyecto1A/ArduinoExtra/ArduinoExtra.ino
+++ Proyecto1A/ArduinoExtra/ArduinoExtra.ino
@@ -1,6 +1,4 @@
 #include <Stepper.h>
-int luzb1[] = {13,11,9,7,5,3,14,16,20,A12,A0,A2,A4,A6,A8,A10};
-int luzb2[] = {12,10,8,6,4,2,15,17,21,A13,A1,A3,A5,A7,A9,A11};
 
 // Declaramos la variable para controlar el servo
 const int spr = 100;
@@ -65,11 +63,6 @@ void setup() {
   Serial1.begin(9600);
   Serial2.begin(9600);
   inputString.reserve(200);
-  
-  for (int i = 0; i<16; i++){
-    pinMode(luzb1[i],OUTPUT);
-    pinMode(luzb2[i],OUTPUT);
-  }
 
   for(int i=0; i<13; i++){
       pinMode(pinesBarreras[i], OUTPUT);
@@ -99,6 +92,8 @@ void setup() {
   TiempoAnterior = millis();
   
   //--------------------------------FIN SETUP LCD---------------------------------
+  barreraEntrada.setSpeed(10);
+  barreraSalida.setSpeed(10);
 }
 
 void serialEvent(){
@@ -141,39 +136,6 @@ void loop(){
       // Desplazamos a la posición 0º
       barreraSalida.step(66);
     }
-  }
-
-  if (valido){
-    String strs[20];
-    int StringCount = 0;
-
-    while (inputString.length() > 0){
-      int index = inputString.indexOf(',');
-      if (index == -1){
-        strs[StringCount++] = inputString;
-        break;
-      }else{
-        strs[StringCount++] = inputString.substring(0, index);
-        inputString = inputString.substring(index+1);
-      }
-    }
-    
-    for (int i = 0; i < 16; i++){
-      Serial1.println(strs[i]);
-      if (strs[i].toInt() == 1){
-        digitalWrite(luzb1[i], HIGH);
-        digitalWrite(luzb2[i], LOW);
-      }else if (strs[i].toInt() == 0){
-        digitalWrite(luzb1[i], LOW);
-        digitalWrite(luzb2[i], HIGH);
-      }else if (strs[i].toInt() == 2){
-        digitalWrite(luzb1[i], HIGH);
-        digitalWrite(luzb2[i], HIGH);
-      } 
-    }
-
-    inputString = "";
-    valido = false;
   }
   //-------------------------------LCD LOOP-------------------------
 
--- Proyecto1A/Proyecto1A.ino
+++ Proyecto1A/Proyecto1A.ino
@@ -1,19 +1,15 @@
 int bloque1[] = {22,23, 24,25, 26,27, 28,29, 30,31, 32,33, 34,35, 36,37};
-int bloque2[] = {38,39, 40,41, 42,43, 44,45, 46,47, 48,49, 50,51, 52,53};
 
 int luzb1[] = {13,11,9,7,5,3,14,16,20,A12,A0,A2,A4,A6,A8,A10};
 int luzb2[] = {12,10,8,6,4,2,15,17,21,A13,A1,A3,A5,A7,A9,A11};
 
 int disponibilidad[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-int disponibilidad2[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
 int alarmaReserva1[] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
-int alarmaReserva2[] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
 
 int buzzer = A14;
 int alarmaAnti = A15;
 bool shute1[] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
-bool shute2[] = {false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false};
 
 String inputString = "";
 bool valido = false;
@@ -28,7 +24,6 @@ void setup() {
   
   for (int i = 0; i<16; i++){ 
     pinMode(bloque1[i],INPUT);
-    pinMode(bloque2[i],INPUT);
   }
 
   for (int i = 0; i<16; i++){
@@ -58,7 +53,7 @@ void loop(){
     }else{
       //Amarillo
       if (digitalRead(bloque1[i]) == HIGH){
-        disponibilidad2[i] = 1;
+        disponibilidad[i] = 1;
         digitalWrite(luzb1[i], HIGH);
         digitalWrite(luzb2[i], LOW);
         shute1[i] = true;
@@ -85,58 +80,12 @@ void loop(){
       digitalWrite(alarmaAnti, LOW);
     }
   }
-  for (int i = 0; i < 16; i++){
-    if (disponibilidad2[i] == 0  && alarmaReserva2[i] == true){
-      digitalWrite(alarmaAnti, HIGH);
-      break;
-    }else{
-      digitalWrite(alarmaAnti, LOW);
-    }
-  }
-
-  //********** Nivel 2*************
-  for (int i = 0; i < 16; i++){
-    if (disponibilidad2[i] != 2 && shute2[i] == false){//|| shute == false
-      if (digitalRead(bloque2[i]) == HIGH){
-        //Rojo
-        disponibilidad2[i] = 1;
-      }else{
-        //Verde
-        disponibilidad2[i] = 0;
-      }    
-    }else{
-      //Amarillo
-      if (digitalRead(bloque2[i]) == HIGH){
-        disponibilidad2[i] = 1;
-        shute2[i] = true;
-        digitalWrite(buzzer, HIGH);
-      }else{
-        disponibilidad2[i] = 2;
-        shute2[i] = false;
-        digitalWrite(buzzer, LOW);
-      }
-    }
-  }
-
-  String lucesNivel2 = "";
-  for (int i = 0; i<15; i++){
-    lucesNivel2.concat(disponibilidad2[i]);
-    lucesNivel2.concat(",");
-  }
-  lucesNivel2.concat(disponibilidad2[15]);
-  lucesNivel2.concat("_");
-  Serial.print(lucesNivel2);
 
   String reporte = "";
   for (int i = 0; i<16; i++){
     reporte.concat(disponibilidad[i]);
     reporte.concat(",");
   }  
-  for (int i = 0; i<15; i++){
-    reporte.concat(disponibilidad2[i]);
-    reporte.concat(",");
-  }  
-  reporte.concat(disponibilidad2[15]);
   Serial1.print(reporte);
   
   delay(100);
@@ -159,12 +108,8 @@ void loop(){
     }
   }
   
-  for (int i = 0; i < 32; i++){
-    if (i<16){
-      disponibilidad[i] = strs[i].toInt();
-    }else{
-      disponibilidad2[i-16] = strs[i].toInt();
-    }
+  for (int i = 0; i < 16; i++){
+     disponibilidad[i] = strs[i].toInt();
   }
   
   delay(500);
