09ecba69969a693e0a5c882c7323ba84598ebc2d
Diego Mazariegos <dmazariegosbarrientos@gmail.com>
2022-12-15 12:19:27 -0600

    barreras con todo

--- Proyecto1A/ArduinoExtra/ArduinoExtra.ino
+++ Proyecto1A/ArduinoExtra/ArduinoExtra.ino
@@ -1,18 +1,21 @@
 int luzb1[] = {13,11,9,7,5,3,14,16,20,A12,A0,A2,A4,A6,A8,A10};
 int luzb2[] = {12,10,8,6,4,2,15,17,21,A13,A1,A3,A5,A7,A9,A11};
 
+int pinesBarreras = {};
+
 String inputString = "";
 bool valido = false;
 
 void setup() {
   Serial.begin(9600);//iniciailzamos la comunicación
   Serial1.begin(9600);
+  Serial2.begin(9600);
   inputString.reserve(200);
   
   for (int i = 0; i<16; i++){
     pinMode(luzb1[i],OUTPUT);
     pinMode(luzb2[i],OUTPUT);
-  }    
+  }
 }
 
 void serialEvent(){
@@ -29,6 +32,34 @@ void serialEvent(){
 
 
 void loop(){
+  if(Serial.available() > 0){
+    entradaBluetooth = Serial.read();
+
+    if(entradaBluetooth == 'A'){
+      digitalWrite(ledBarreraEntrada, HIGH);
+      // Desplazamos a la posición 90º
+      barreraEntrada.step(-66);
+    }
+
+    if(entradaBluetooth == 'B'){
+      digitalWrite(ledBarreraEntrada, LOW);
+      // Desplazamos a la posición 0º
+      barreraEntrada.step(66);
+    }
+
+    if(entradaBluetooth == 'D'){
+      digitalWrite(ledBarreraSalida, HIGH);
+      // Desplazamos a la posición 90º
+      barreraSalida.step(-66);
+    }
+
+    if(entradaBluetooth == 'E'){
+      digitalWrite(ledBarreraSalida, LOW);
+      // Desplazamos a la posición 0º
+      barreraSalida.step(66);
+    }
+  }
+
   if (valido){
     String strs[20];
     int StringCount = 0;
